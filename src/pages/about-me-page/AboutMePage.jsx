86c603cd1aa8ade85067539fc1dc68afaaeb135c
EJ <estanislaojose.suarez@eclectustech.net>
2024-07-16 22:18:15 +0800

    fix - adjust about me

--- src/pages/about-me-page/AboutMePage.jsx
+++ src/pages/about-me-page/AboutMePage.jsx
@@ -9,42 +9,29 @@ const AboutMePage = () => {
         <h1>About Me</h1>
         <p>
           I am a passionate computer engineering graduate with Latin honors,
-          driven by a lifelong love for programming and software development,
-          which began during my high school years. During my internship and
-          self-paced learning, I earned certifications in web development and
+          driven by a lifelong love for programming and software development.
+          During my internship, I earned certifications in web development and
           cloud computing, further fueling my enthusiasm for technology.
         </p>
         <p>
-          For nearly a year, I have been honing my skills as a junior software
-          engineer, in a local company, here in the Philippines. My primary responsibilities include
-          coding and implementing new features for a timekeeping app and
-          website. Additionally, I had the privilege of serving as the lead
-          programmer on a viral prototype capstone project during my college
-          years, where we developed a firefighting robot capable of autonomously
-          extinguishing fires using a fire extinguisher.
+          Currently, I work as a junior software engineer in the Philippines,
+          focusing on coding and implementing new features for a timekeeping app
+          and website. My college experience includes being the main developer
+          of a prototype, a firefighting robot that is capable of autonomously
+          extinguishing fires, and it went viral online.
         </p>
         <p>
-          I thrive on challenges that push me to learn and grow, especially
-          projects that involve creating interactive and user-friendly web
-          applications. One of my proudest achievements remains our viral
-          prototype project, which garnered significant attention online.
+          I enjoy creating interactive and user-friendly web applications,
+          specializing in Angular and React frameworks. Proficient in
+          JavaScript, TypeScript, and Bootstrap, I also have foundational
+          knowledge of Python, C#, and Java. I am dedicated to continuous
+          learning and collaboration, eager to expand my skill set in evolving
+          technologies.
         </p>
         <p>
-          Technically, I am proficient in Angular and React frameworks, with
-          strong skills in JavaScript, TypeScript, and Bootstrap. I also possess
-          foundational knowledge of Python, C#, and Java. My eagerness to learn
-          new technologies, coupled with my adaptability and collaborative
-          spirit, drives me to continually expand my skill set.
-        </p>
-        <p>
-          Looking ahead, my aspirations in web development are anchored in
-          continuous improvement and staying abreast of emerging technologies. I
-          aspire to transition into strategic roles such as project management,
-          where I can leverage my technical expertise to lead teams and align
-          technical solutions with overarching business objectives. Ultimately,
-          I see myself evolving into roles like technical lead or product
-          manager, where I can play a pivotal role in shaping the future of
-          innovative projects and driving technological advancements.
+          Looking forward, I aim to advance into project management roles,
+          leveraging my technical expertise to lead teams and drive innovative
+          projects aligned with business objectives.
         </p>
       </div>
     </div>
